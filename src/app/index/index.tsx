c38f1368912795125f6ea7956b8eb1a8fe8ba789
Rafael Toledo <toledorafael304@gmail.com>
2024-03-06 19:13:25 -0300

    feat: create header of page

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -5,8 +5,10 @@ import { styles } from "./styles";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Escolha os produtos</Text>
-      <Text style={styles.subtitle}>
+      <Text style={styles.title}>Escolha {"\n"}
+        <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+      <Text style={styles.message}>
         Descubra receitas baseadas nos produtos que você escolheu
       </Text>
     </View>
