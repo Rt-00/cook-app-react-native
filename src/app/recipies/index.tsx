67304f206196830546c676c190a533c68308820c
Rafael Toledo <toledorafael304@gmail.com>
2024-03-06 22:02:48 -0300

    "feat: create the recipie screen"

---
+++ src/app/recipies/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FlatList, Text, View } from "react-native";
+import { styles } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { Recipe } from "@/components/Recipe";
+
+export default function Recipies() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <MaterialIcons
+          name="arrow-back"
+          size={32}
+          onPress={() => router.back()}
+        />
+
+        <Text style={styles.title}>Ingredientes</Text>
+
+        <FlatList
+          data={["1"]}
+          keyExtractor={(item) => item}
+          renderItem={() => (
+            <Recipe
+              recipe={{
+                name: "Omelete",
+                image:
+                  "https://img.cybercook.com.br/receitas/105/omelete-classica-1.jpeg",
+                minutes: 10,
+              }}
+            />
+          )}
+        />
+      </View>
+    </View>
+  );
+}
