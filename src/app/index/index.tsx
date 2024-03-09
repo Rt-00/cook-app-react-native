3b2fd5583c021490b16be62e0db1cf95a87c4c9c
Rafael Toledo <toledorafael304@gmail.com>
2024-03-09 12:42:20 -0300

    feat: refactor index.tsx component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Alert } from "react-native";
 import "./styles";
 import { styles } from "./styles";
-import { Ingredients } from "@/components/Ingredients/Ingredients";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { services } from "@/services";
+import { Ingredient } from "@/components/Ingredient/Ingredient";
+import { router } from "expo-router";
+import { SelectedsCounter } from "@/components/SelectedsCounter/SelectedsCounter";
 
 export default function Index() {
+  const [selected, setSelected] = useState<string[]>([]);
+  const [ingredients, setIngredients] = useState<IngredientResponse[]>([]);
+
+  const handleClearSelecteds = () => {
+    Alert.alert("Limpar", "Deseja limpar tudo?", [
+      { text: "Não", style: "cancel" },
+      {
+        text: "Sim",
+        onPress: () => setSelected([]),
+      },
+    ]);
+  };
+
+  const handleSearchSelecteds = () => {
+    router.navigate("/recipes/" + selected);
+  };
+
+  const handleToggleSelected = (value: string) => {
+    if (selected.includes(value)) {
+      return setSelected((state) => state.filter((item) => item !== value));
+    }
+
+    setSelected((state) => [...state, value]);
+  };
+
+  useEffect(() => {
+    services.ingredientes.findAll().then(setIngredients);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -15,7 +47,30 @@ export default function Index() {
         Descubra receitas baseadas nos produtos que você escolheu
       </Text>
 
-      <Ingredients />
+
+      <ScrollView
+        contentContainerStyle={styles.ingredients}
+        showsVerticalScrollIndicator={false}
+      >
+        {ingredients.map((item) => (
+          <Ingredient
+            key={item.id}
+            name={item.name}
+            image={`${services.storage.imagePath}/${item.image}`}
+            selected={selected.includes(item.id)}
+            onPress={() => handleToggleSelected(item.id)}
+          />
+        ))}
+      </ScrollView>
+
+      {selected.length > 0 && (
+        <SelectedsCounter
+          quantity={selected.length}
+          onClear={handleClearSelecteds}
+          onSearch={handleSearchSelecteds}
+        />
+      )}
+
     </View>
   );
 }
